29327be79af9573294dfb2272eeba01023d0b3cc
stephencarter-io <stephen@stephencarter.io>
2017-12-29 10:22:00 +1000

    Finish lecture 106

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+	public float padding = 0.5f;
+	private float xmin;
+	private float xmax;
+
+	// Use this for initialization
+	void Start () {	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
