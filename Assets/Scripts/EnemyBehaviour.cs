f8a1af2c5121577ba51c7756e35c58c21d950eb9
stephencarter-io <stephen@stephencarter.io>
2017-12-29 14:19:56 +1000

    Finish lecture 112

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+	private void OnTriggerEnter2D(Collider2D col) {
+		Projectile missile = col.gameObject.GetComponent<Projectile>();
+		if (missile) {
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <=0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+}
